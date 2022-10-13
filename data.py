baeffcf6469b51ae10fd632147ea593e462aa88e
abaduna <artuelrey@gmail.com>
2022-10-13 18:01:08 -0300

    first commit

---
+++ data.py
@@ -0,0 +1,10 @@
+        #Es necesario importar las depencendias necesarias
+from datetime import date
+from datetime import datetime
+
+#Día actual
+
+new_date = datetime(2022, 11, 20, 0)
+hoy= datetime.now()
+print('La Fecha de Hoy es:', hoy- new_date)
+
